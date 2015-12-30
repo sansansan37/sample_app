e42008c62178553a5e2ffa00b8ff08bc69a43dcf
san37 <sanaecopa@yahoo.co.jp>
2015-12-30 17:47:47 +0000

    Display a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
-  devise_for :users
+  get "users/show"
+  devise_for  :users
+  resources :users, only: [:show]
   root  'static_pages#home'
+ 
  match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
